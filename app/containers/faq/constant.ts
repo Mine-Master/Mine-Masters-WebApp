42407cc2a6fca083a3a4414a611b86b14b30338b
moji <mojtabaparvar@gmail.com>
2024-05-01 16:07:14 +0330

    chore: add FAQs

--- app/containers/faq/constant.ts
+++ app/containers/faq/constant.ts
@@ -2,31 +2,32 @@ export const FAQ_ITEMS = [
   {
     title: "What is Mine Masters?",
     description:
-      "Mine Masters is a play-to-earn game that allows players to earn cryptocurrency by playing the game. Players can earn by mining resources, crafting items, and trading them in the marketplace.",
+      "MineMasters is a crypto-based game that combines the thrill of gaming with cryptocurrency rewards. Players purchase lands, mine resources, engage in warfare, and trade NFTs to build their empire and earn in-game tokens.",
   },
   {
-    title: "What is the game about?",
+    title: "How do I start playing MineMasters?",
     description:
-      "Mine Masters is a play-to-earn game that allows players to earn cryptocurrency by playing the game. Players can earn by mining resources, crafting items, and trading them in the marketplace.",
+      "To begin, players need to purchase a land NFT during one of the scheduled land releases. After acquiring land, players can buy miner NFTs to start mining game tokens.",
   },
   {
-    title: "How do I play Mine Masters?",
+    title: "Can I choose not to participate in wars?",
     description:
-      "Mine Masters is a play-to-earn game that allows players to earn cryptocurrency by playing the game. Players can earn by mining resources, crafting items, and trading them in the marketplace.",
+      "Yes, players can choose between being in 'war mode' or 'peace mode'. War mode allows players to attack others and risk being attacked, while peace mode focuses on development and resource management without the risk of attacks.",
   },
   {
-    title: "How do I earn cryptocurrency?",
+    title:
+      "What are the costs associated with switching between war and peace modes?",
     description:
-      "Mine Masters is a play-to-earn game that allows players to earn cryptocurrency by playing the game. Players can earn by mining resources, crafting items, and trading them in the marketplace.",
+      "Switching modes is a strategic decision that costs about 20% of your game tokens each time you switch.",
   },
   {
-    title: "How do I earn cryptocurrency?",
+    title: "How are mining rewards distributed?",
     description:
-      "Mine Masters is a play-to-earn game that allows players to earn cryptocurrency by playing the game. Players can earn by mining resources, crafting items, and trading them in the marketplace.",
+      "Mining rewards are given out in the game’s token and are distributed every 7 days. However, the mining rewards are halved every 6 months to maintain the game's economic balance.",
   },
   {
-    title: "How do I earn cryptocurrency?",
+    title: "What happens if my land is attacked?",
     description:
-      "Mine Masters is a play-to-earn game that allows players to earn cryptocurrency by playing the game. Players can earn by mining resources, crafting items, and trading them in the marketplace.",
+      "If your land is attacked, you may incur soldier casualties and structural damages, which can be repaired using in-game resources. These dynamics add realism and strategic depth to the game.",
   },
 ];
